6256a2b80051949af991dc0804c2d896989016ee
Jiya Agrawal <jiyaagrawal6395@gmail.com>
2026-03-23 10:55:17 +0530

    Uncomment and organize claim routes (#12)
    Refactor claimRoutes.js to uncomment and organize user and admin routes.

--- server/routes/claimRoutes.js
+++ server/routes/claimRoutes.js
@@ -1,29 +1,4 @@
-// const express = require("express");
-// const router = express.Router();
 
-// const {
-//   createClaim,
-//   getClaims,
-//   updateClaimStatus,
-//   deleteClaim
-// } = require("../controllers/claimController");
-
-// const auth = require("../middleware/authMiddleware");
-// const admin = require("../middleware/adminMiddleware");
-
-// // ==============================
-// // USER ROUTES
-// // ==============================
-// router.post("/", auth, createClaim);
-// router.get("/", auth, getClaims);
-// router.delete("/:id", auth, deleteClaim);
-
-// // ==============================
-// // ADMIN ROUTES
-// // ==============================
-// router.patch("/:id/status", auth, admin, updateClaimStatus);
-
-// module.exports = router;
 const express = require("express");
 const router  = express.Router();
 const auth    = require("../middleware/authMiddleware");
@@ -36,7 +11,6 @@ const {
   updateClaimStatus
 } = require("../controllers/claimController");
 
-const auth = require("../middleware/authMiddleware");
 const adminOnly = require("../middleware/adminMiddleware");
 
 //User routes
