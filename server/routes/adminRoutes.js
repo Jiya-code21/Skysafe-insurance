cdaa279f4de86625a052bc61b59441292e4ffa3e
Jiya Agrawal <jiyaagrawal6395@gmail.com>
2026-03-24 10:06:11 +0530

    Remove unused middleware imports from adminRoutes
    Removed unused middleware imports for auth and admin.

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../middleware/authMiddleware");
-const admin = require("../middleware/adminMiddleware");
-
 const {
   createAdmin,
   adminLogin,
